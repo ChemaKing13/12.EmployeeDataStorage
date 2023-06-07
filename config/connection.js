62e6379dfe4c20c2bf3e29bd93e881eb432b2665
ChemaKing13 <jm2000ho@gmail.com>
2023-06-07 17:20:31 -0600

    connection.js added

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Connection to mysql
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const dbConnection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: 3306
+});
+
+dbConnection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to database:', error);
+  } else {
+    console.log('Connected to employee_db.');
+  }
+});
+
+module.exports = dbConnection;
---
+++ prompts/index.js
@@ -0,0 +1,47 @@
+const inquirer = require('inquirer'); 
+
+function startApp() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?', 
+                choices: [
+                    'View all employees',
+                    'Add employee', 
+                    'Update employee role', 
+                    'View all roles', 
+                    'View all departments', 
+                    'Add department', 
+                    'Quit', 
+                ],
+            },
+        ])
+    .then((answers) => {
+        switch (answers.action) {
+            case 'View all employers': 
+                break; 
+            case 'Add employee':
+                break; 
+            case 'Update employee role': 
+                break; 
+            case 'View all roles': 
+                break; 
+            case 'Add role':
+                break; 
+            case 'View all departments': 
+                break; 
+            case 'Add department': 
+                break; 
+            case 'Quit': 
+                console.log('Goodbye!'); 
+                process.exit(0);  
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error); 
+    }); 
+}
+
+startApp(); 
