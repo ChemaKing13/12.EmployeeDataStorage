9c5b01e4f17f920c7aa1dddf6341067885184bb9
ChemaKing13 <jm2000ho@gmail.com>
2023-06-08 16:16:09 -0600

    figlet art added

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const { startApp } = require('./app'); 
+const { startApp } = require('./app');
+const figlet = require('figlet');
 
-startApp(); 
+// Function to display the project name as ASCII art
+function displayProjectName() {
+  const projectName = 'EmployeeData';
+
+  figlet(projectName, function (err, data) {
+    if (err) {
+      console.log('Error occurred while generating ASCII art:', err);
+      return;
+    }
+    console.log(data);
+    console.log('\n');
+    startApp(); // Call the startApp function after displaying the project name
+  });
+}
+
+// Call the displayProjectName function
+displayProjectName();
+
+
